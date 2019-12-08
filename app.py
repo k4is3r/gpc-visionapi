6e53f593ac5a57e0424c6462d5ffd66235d2e84d
Eduardo Imery <ejwimery@gmail.com>
2019-12-07 19:51:35 -0600

    testing with other images

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ client = vision.ImageAnnotatorClient()
 
 file_name = os.path.join(
     os.path.dirname(__file__),
-    'car.jpg')
+    'fish.jpg')
 
 with io.open(file_name,'rb') as image_file:
     content = image_file.read()
