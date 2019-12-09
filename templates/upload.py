bd546e5f78afee70891cde9ae020fb224ec53010
Eduardo Imery <ejwimery@gmail.com>
2019-12-09 16:50:28 -0600

    creating function upload_file and uploader

--- templates/upload.py
+++ templates/upload.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template, request
 from werkzeug import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOADER_FOLDER'] ="./images"
+app.config['UPLOAD_FOLDER'] ="./images"
 
- 
+@app.route("/")
+def upload_file():
+    return render_template('formulario.html')
+
+@app.route("/uploader", method=['POST'])
+def uploader():
+    if request.method == 'POST':
+        f = request.files['archivo']
+        filename= secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) 
+        return "File upload complete"
