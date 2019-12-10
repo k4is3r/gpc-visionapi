cadc0c2773c23adc7dabf9cca32b34f30ab11479
Eduardo Imery <ejwimery@gmail.com>
2019-12-09 20:41:53 -0600

    fixing problems

--- upload.py
+++ upload.py
@@ -1,21 +1,50 @@
 import os
-from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, send_from_directory
+import app 
+from app import analyze_photo
+
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] ="./images"
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+description_list = []
 
 @app.route("/")
-def upload_file():
-    return render_template('./formulario.html')
+def index():
+    return render_template("formulario.html")
 
-@app.route("/uploader", methods=['POST'])
-def uploader():
-    if request.method == 'POST':
-        f = request.files['archivo']
-        filename= secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) 
-        return "File upload complete"
+@app.route("/upload", methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, 'images')
+    print(target)
+    
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    else:
+        print("Couldn't create upload directory: {}".format(target))
+     	
+    file_list = request.files.getlist("file")
+    for file in file_list:
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)
+        file.save(destination)
+    print(filename)
+    description_list = analyze_photo(destination)
+    for desc in description_list:
+        print(desc.description)
+     
+    return render_template("complete.html", image_name=filename)
+
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
+    #port=4550
