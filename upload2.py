ba0b725091e286e7c9e6648e263024f93bb6ba92
Eduardo Imery <ejwimery@gmail.com>
2019-12-09 18:02:51 -0600

    creating functions to upload files

--- upload2.py
+++ upload2.py
@@ -1,9 +1,24 @@
 import os
 from flask import Flask, render_template, request
 
-app.Flask(__name__)
+app = Flask(__name__)
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/")
 def index():
     return render_template("formulario.html")
+
+@app.route("/upload")
+def upload():
+    target = os.path.join(APP_ROOT, 'images/')
+    print(target)
+    
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    file_list = request.file.getlist("file")
+    for file in file_list:
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
